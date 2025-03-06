27904e14fec57fede5cf0234c27946f97e20f062
Noctis, Kei Hong Yeung <yeungkeihong@gmail.com>
2025-03-06 22:06:46 +0800

    feat (code): Add question 647

---
+++ 647.palindromic-substrings.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=647 lang=python3
+#
+# [647] Palindromic Substrings
+#
+
+# @lc code=start
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        result = []
+        for idx in range(len(s)):
+            left,right = 0,idx + 1
+            while left < len(s) and right <= len(s):
+                sub_string = s[left:right]
+                if sub_string == sub_string[::-1]:
+                    result.append(sub_string)
+                left += 1
+                right += 1
+        return len(result)
+
+# @lc code=end
+
+if __name__ == '__main__':
+    s = Solution()
+    s.countSubstrings('dbabcccbcdbbbadabbdabaabcbbabaacdadcdbbbbdddbcbbbcbcabacacdaadaadcdccbacdaadadcbaacabbddabdadcabbccadacadaaacbbddaaababacaabbbacaccbcbbabddbbcccaaacbcdcabcbacdbddcdcadaaadcbccbbcabbcbdaadcbddaacacdadacbbdabcdcdadccaccdbdddddcabdbcdbaadacadadbabdcdbbadaacdbadcdabdbbcabbbdaacaaaaadcaabaaaadabaaddcaabdddcbcadcbdbbdbcbcdbadcadacbbcdccddaccccacbacaacbbdadadcacabdabadbbcdbcaaccdbdcabcadbacbccddbabbdacbcbbcbcabcacdaacccaadcdbdccabcaaacaddadcabacdccaaaddaaadbccdbbcccdddababcdbcddcbdddbbbdaadaccbcaabbcbdbadbabbacdbbbdaaccdcabddacadabcccacdabacbcdbcbdabddacacbdbcaacacaabbaaccddabaadbbaabaddbcacbacdbbbacdccabbcdbbbdbdbbcacabdddbdbaaabbcdcbabcbbbccdcdcdcaaddadccbabbacaddcaddcadcdccaccacabbaababdbbcbdcdccccccdadbdbdcdbdadcdcacdaabaacabaacdacdbaaccadbcddbdccabbcabcadcbdadbcdadbbbccacbcbbcaaaabdacbadacaadcadaacdacddcbbabdacacaabccdaccbbcbbcbcaacdabdcbcdbccdbcbcbddaacdacaaaddcaddcadccacbaddbbbccbbbcbbcbadcabbccbbaadaacacabddbdbddcadbdaaccadbcccabdcdbadbbacbcbbcdcabcddcddddabddbdabdcabdbdbbbcdbcdabbdcb')
